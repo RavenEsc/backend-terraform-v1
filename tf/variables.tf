2f075f985800465073463e416b1b9f91f650b063
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2024-05-06 12:11:30 -0500

    restructured

---
+++ tf/variables.tf
@@ -0,0 +1,19 @@
+variable "reg" {
+  default = "us-east-1"
+}
+
+variable "accountnumber" {
+  default = "464004139021"
+}   
+
+variable "tablename" {
+  default = "Visitor-Counter"
+}
+
+variable "hashkey" {
+  default = "Countname"
+}
+
+variable "code_dir" {
+  default = "../code"
+}
